020873714299c9b6416469898df859fd84fc3a7b
tlgbltcn <tbolatcan@gmail.com>
2019-08-21 21:58:03 +0300

    update app gradle versions

--- app/src/main/java/com/tlgbltcn/app/weather/AppConstant.kt
+++ app/src/main/java/com/tlgbltcn/app/weather/AppConstant.kt
@@ -2,16 +2,16 @@ package com.tlgbltcn.app.weather
 
 object AppConstant {
 
-    val BASE_URL = "https://api.openweathermap.org"
-    val API_KEY = "29126df019f086747b1870a31b2e8bc3"
-    val WEATHER_ICON_URL = "http://openweathermap.org/img/w/"
+    const val BASE_URL = "https://api.openweathermap.org"
+    const val API_KEY = "29126df019f086747b1870a31b2e8bc3"
+    const val WEATHER_ICON_URL = "http://openweathermap.org/img/w/"
 
-    val REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key"
-    val LOCATION_KEY = "location-key"
-    val LAST_UPDATED_TIME_STRING_KEY = "last-updated-time-string-key"
-    val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
-    val REQUEST_CHECK_SETTINGS = 10
+    const val REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key"
+    const val LOCATION_KEY = "location-key"
+    const val LAST_UPDATED_TIME_STRING_KEY = "last-updated-time-string-key"
+    const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    const val REQUEST_CHECK_SETTINGS = 10
 
-    val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 10000
-    val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2
+    const val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 10000
+    const val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2
 }
